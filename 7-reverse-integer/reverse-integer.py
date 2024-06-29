aa8791da164dd20a860be38d1ad3cc4d6949d23c
SamuelMersha <115375666+SamuelMersha@users.noreply.github.com>
2024-06-29 12:22:54 +0300

    Time: 14 ms (100.00%) | Memory: 16.7 MB (9.75%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverse(self, x: int) -> int: 
+        k = 1
+        if x < 0:
+            x = -x
+            k = -1
+        y = str(x)
+        re = 0
+        for i in range(len(y)):
+            re = re +  int(y[i]) * 10**(i) 
+        print(re)
+        if re > -(2**31) and re < 2**31 - 1:
+            return k*re
+        else:
+            return 0
